907ca5ea72e0429d54c8220cb9d7e462723b31f3
Numaan <nomaan.ahmad21@gmail.com>
2022-12-21 02:24:51 +0530

    Added NextPermutation.java

--- TwoPointer/NextPermutation.java
+++ TwoPointer/NextPermutation.java
@@ -1,5 +1,38 @@
 package TwoPointer;
 
 public class NextPermutation {
-    
+    public void nextPermutation(int[] nums) {
+        final int size = nums.length;
+        
+        int i = size-2;
+        boolean flag = false;
+        for (; i >= 0; i--) {
+            if (nums[i] < nums[i+1]) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) {
+            reverse(nums, 0, size-1);
+            return;
+        }
+
+        for (int j = size-1; j >= 0; j--)
+            if (nums[j] > nums[i]) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+
+        reverse(nums, i+1, size-1);
+    }
+
+    private void reverse (int[] nums, int from, int to) {
+        while (from < to) {
+            int temp = nums[to];
+            nums[to--] = nums[from];
+            nums[from++] = temp;
+        }
+    }
 }
