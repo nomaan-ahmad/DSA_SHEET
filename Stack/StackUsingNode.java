10f4a782f0023be429f1aa854e7b3a8318e8b5f1
Numaan <nomaan.ahmad21@gmail.com>
2022-12-20 18:43:13 +0530

    Create StackUsingNode.java

---
+++ Stack/StackUsingNode.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class StackUsingNode {
+    private static class Node<T> {
+        T val;
+        Node<T> next;
+
+        Node(T _val) {
+            val = _val;
+            next = null;
+        }
+    }
+    public static class Stack<T> {
+        private Node<T> top;
+        private int size;
+        Stack(){
+            top = null;
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return top == null;
+        }
+
+        public T peek() throws Exception {
+            if (top == null) throw new NullPointerException();
+
+            return top.val;
+        }
+
+        public void push(T val) {
+            Node<T> temp = new Node<>(val);
+            size++;
+            if (!isEmpty()) temp.next = top;
+            top = temp;
+        }
+
+        public T pop() throws Exception {
+            if (top == null) throw new NullPointerException();
+
+            size--;
+            T temp = peek();
+            top = top.next;
+            return temp;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        @Override
+        public String toString() {
+            Object[] res = new Object[size];
+            Node<T> temp = top;
+
+            int i = 0;
+            while (i < size) {
+                res[i] = temp.val;
+                temp = temp.next;
+            }
+
+            return Arrays.toString(res);
+        }
+    }
+}
