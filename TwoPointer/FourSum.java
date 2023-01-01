9dd1e89362ff865b2f210fe9df758151ef3773f4
Numaan <nomaan.ahmad21@gmail.com>
2023-01-01 23:46:50 +0530

    Create FourSum.java

---
+++ TwoPointer/FourSum.java
@@ -0,0 +1,9 @@
+public class FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len < 4) return res;
+
+        return res;
+    }
+}
