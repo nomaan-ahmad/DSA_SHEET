64d211cf10337c0863e15a2a66ef77c036cc3b14
nomaan-ahmad <nomaan.ahmad21@gmail.com>
2023-03-27 23:50:07 +0530

    Solved Alien Dictionary using Topological Sort

---
+++ Graph/Topological Sorting/Problems/AlienDictionary.java
@@ -0,0 +1,6 @@
+// https://practice.geeksforgeeks.org/problems/alien-dictionary/1
+public class AlienDictionary {
+    public String findOrder(String [] dict, int N, int K) {
+        return "";
+    }
+}
